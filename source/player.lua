e0ed3b3145fb64a7cbfb9f3a943e0941c75de603
Liam <liamreadsspam@gmail.com>
2021-08-14 00:06:50 -0300

    Draw blue diamond inside player

--- source/player.lua
+++ source/player.lua
@@ -12,15 +12,28 @@ function Player:new(x, y)
 end
 
 function Player:draw()
-    love.graphics.setColor(0.25, 0.5, 1, 1)
+    local drawn_position = self.drawn_position
     if Transition.flag then
-        Transition.shader:send("pos", {self.drawn_position.x, self.drawn_position.y})
+        Transition.shader:send("pos", {drawn_position.x, drawn_position.y})
         love.graphics.setShader(Transition.shader)
     end
 
     love.graphics.setLineWidth(4)
-    love.graphics.rectangle("line", self.drawn_position.x + self.offset.x,
-        self.drawn_position.y + self.offset.y, self.width, self.width)
+    love.graphics.rectangle("line", drawn_position.x + self.offset.x,
+        drawn_position.y + self.offset.y, self.width, self.width)
+    love.graphics.setColor(0.25, 0.5, 1, 1)
+    love.graphics.setLineWidth(3)
+
+    local x_mid = drawn_position.x + TILE_WIDTH/2
+    local y_mid = drawn_position.y + TILE_WIDTH/2
+    local diamond_offset = 2
+    local vertices = {
+        x_mid, drawn_position.y + TILE_WIDTH/4 + diamond_offset,
+        drawn_position.x + 3/4 * TILE_WIDTH - diamond_offset, y_mid,
+        x_mid, drawn_position.y + 3/4 * TILE_WIDTH - diamond_offset,
+        drawn_position.x + TILE_WIDTH/4 + diamond_offset, y_mid
+    }
+    love.graphics.polygon("line", vertices)
 
     love.graphics.setShader()
     love.graphics.setColor(1, 1, 1)
